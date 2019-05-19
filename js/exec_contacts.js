72bf4371d39982e145276791851ad2d93867da6b
Ola Eleyinmi <eleyinmiolasunkanmi@gmail.com>
2019-05-19 23:07:16 +0100

    Created an 'edit contact form' that displays on the contact modal and card on clicking the edit button

--- js/exec_contacts.js
+++ js/exec_contacts.js
@@ -42,18 +42,26 @@ $(document).ready(function() {
     contactRows.click(function() {
         $(".mainTable").css("width", "48%");
         $("#contactCard").css({"right": "30px", "opacity": "1"});
-        $("body").css("padding-right", "0");
 
         var rowIndex = $(this).attr("data-rowindex"); // Gets the index of the row clicked 
         parseInt(rowIndex); // Converts the string to number
 
         // rowIndex of the clicked row is used to access the contactsData
         // and dynamically display the Contact's details
-        $(".contactName").text(contactsData.contacts[rowIndex].name);
+        $(".contactName").text(contactsData.contacts[rowIndex].firstname + " " + contactsData.contacts[rowIndex].lastname);
         $(".contactEmail").text(contactsData.contacts[rowIndex].email);
         $(".contactPhone").text(contactsData.contacts[rowIndex].phone);
         $(".contactCompany").text(contactsData.contacts[rowIndex].company);
         $(".contactCountry").text(contactsData.contacts[rowIndex].country);
+    
+        // Displays the current contact's details on the 'edit contact form'
+        $(".editTitle").text("Edit " + contactsData.contacts[rowIndex].firstname + " " + contactsData.contacts[rowIndex].lastname + "'s details");
+        $(".editFirstNameField").val(contactsData.contacts[rowIndex].firstname);
+        $(".editLastNameField").val(contactsData.contacts[rowIndex].lastname);
+        $(".editEmailField").val(contactsData.contacts[rowIndex].email);
+        $(".editPhoneField").val(contactsData.contacts[rowIndex].phone);
+        $(".editCompanyField").val(contactsData.contacts[rowIndex].company);
+        $(".editCountryField").val(contactsData.contacts[rowIndex].country);
     });
 
     // Function that closes contact card for medium screen size and above
@@ -80,6 +88,18 @@ $(document).ready(function() {
         $("#sidenavPC").removeClass("col-md-1").addClass("col-md-3");
         $(this).hide();
         $("#contractBtn").show();
-        $(".sidenavText").show();
+        $(".sidenavText").show("slow");
+    });
+
+    // Replace the contact card and modal with an 'edit contact form' on clicking the edit button
+    $(".editBtn").click(function(){
+        $(".modal-card-contact-details").hide("slow");
+        $(".editContactForm").show("slow");
+    });
+
+    // Replace the 'edit contact form' with the contact card and modal on clicking the cancel button
+    $(".cancelEditBtn").click(function(){
+        $(".editContactForm").hide("slow");
+        $(".modal-card-contact-details").show("slow");
     });
 });
